690a6751453ed20a9414ea806513f169b305b79c
Adhish Kapoor <adhish.kapoor@yahoo.co.in>
2018-01-31 12:05:24 +0530

    Update LinkedList.cs

--- LinkedList.cs
+++ LinkedList.cs
@@ -10,8 +10,54 @@ namespace LinkedList1
 
             public node link { get; set; }
 
-            public string info { get; set; }
+            public int info { get; set; }
 
+        }
+        public static node mergelists(node a,node b)
+        {
+            if (a == null)
+                return b;
+            if (b == null)
+                return a;
+           node temp = null;
+
+            if(a.info<=b.info)
+            {
+                temp = a;
+                temp.link = mergelists(a.link, b);
+
+            }
+            else
+            {
+                temp = b;
+                temp.link = mergelists(a, b.link);
+            }
+            return temp;
+        }
+        public static node middle(node temp)
+        {
+            node fast = temp;
+            node slow = temp;
+            while(fast.link!=null && fast.link.link!=null)
+            {
+                slow = slow.link;
+                fast = fast.link.link;
+
+            }
+            return slow;
+        }
+        public static node mergesort(node temp)
+        {
+            if (temp == null || temp.link == null)
+                return temp;
+            node mid = middle(temp);
+            node a = temp;
+            node b = mid.link;
+            mid.link = null;
+            a = mergesort(a);
+            b = mergesort(b);
+
+            return mergelists(a, b);
         }
         public static void delete(ref node temp,int pos,ref node start)
         {
@@ -52,13 +98,16 @@ namespace LinkedList1
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            int pos = int.Parse(Console.ReadLine());//pos to delete element
+            //int pos = int.Parse(Console.ReadLine());//pos to delete element
             string text = Console.ReadLine();
 
 
             string[] numbers = text.Split(' ');
-            
-
+            int[] arr = new int[numbers.Length];
+            for(int i=0;i<numbers.Length;i++)
+            {
+                arr[i] = int.Parse(numbers[i]);
+            }
             node temp;
             node temp2 = new node();
             node start = new node();
@@ -66,7 +115,7 @@ namespace LinkedList1
             for (int i = 0; i < numbers.Length; i++)
             {
                 temp = new node();
-                temp.info = numbers[i];
+                temp.info = arr[i];
                 temp.link = null;
 
                 if (i == 0)
@@ -83,8 +132,9 @@ namespace LinkedList1
             }
             temp = start;
             // newhead = reverse(temp);
-             delete(ref temp, pos,ref start);
-            newhead = start;
+            //delete(ref temp, pos,ref start);
+            newhead = mergesort(temp);
+            
             while (newhead != null)
             {
                 Console.Write(newhead.info + " ");
